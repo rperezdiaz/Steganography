52c530002ed6e37d1377834a3a49a419fda7a312
Rey Perez <rey.perez2@upr.edu>
2022-06-02 00:07:53 -0400

    version-2

--- image_steganography.py
+++ image_steganography.py
@@ -1,57 +1,125 @@
-import sys
+import os
 
-def toBinary(a):
-  l,m=[],[]
+def extract(key, stego_img):
+    bitstring = ''
 
-  for i in a:
-    l.append(ord(i))
-  for i in l:
-    m.append(int(bin(i)[2:]))
-  return m[0]
+    for i in range(4, len(key)):
+        stego_value = int(stego_img[i])
+        key_value = int(key[i])
+        bitstring += str( stego_value - key_value )
 
-def embed_imgs(carrier, legit_msg):
+    #separate into groups of 8
+    n=8
+    out = [(bitstring[i:i+n]) for i in range(0, len(bitstring), n)]
+
+    bytes = []
+    for i in range(len(out)):
+        bytes.append(int(out[i],2))
+
+    return bytes
+
+def embed(carrier, msg):
     stego_img = []
     #add header of carrier ppm to stego_img 
     for i in range(4):
         stego_img.append(carrier[i])
 
     c_idx = 4
-    l_idx = 0
-    while(l_idx < len(legit_msg)):
-        byte = toBinary(legit_msg[l_idx])
-        bits = list(str(byte))
-        for i in range (len(bits)):
-            value = int(carrier[c_idx])
-            bit = int(bits[i])
-            stego_img.append( str( value + bit) ) 
-            c_idx += 1
-        l_idx += 1
-
+    for i in range(len(msg)):
+        byte = msg[i][2:]
+        while(len(byte)!= 8):
+            byte = '0'+byte
+        for bit in byte:
+            c_value = int(carrier[c_idx])
+            value = int(bit)
+            stego_img.append(str(c_value + value))
+            c_idx +=1
     for i in range(c_idx, len(carrier)):
         stego_img.append(carrier[i])
-    
-    # print(stego_img)
-
 
     return stego_img
+
+def get_ppm_as_byte_list(image_file_name):
+    f = open(sys.path[0] + '/UPRB/images/'+ image_file_name, "r")
+    content = f.read()
+    byte_array = bytearray(content, "utf8")
+    byte_list = []
+
+    for byte in byte_array:
+        binary_representation = bin(byte)
+        byte_list.append(binary_representation)
+    
+    f.close()
+    return byte_list
 
 def get_ppm_as_list(image_file_name):
     f = open(sys.path[0] + '/UPRB/images/'+ image_file_name, "r")
     content = f.read()
-    list = content.split()
+
+    arr = content.split()
+    
     f.close()
-    return list
+    return arr
 
-def main(): 
-    legit_msg = get_ppm_as_list("cotorra_boricua.ppm")
-    carrier = get_ppm_as_list("gato.ppm")
+def reveal_image():
+    stego_name = input("Enter Segoimage name (with extension): ")
+    key_name = input("Enter key image name (with extension): ")
 
-    f = open("Steganography\stegoimage.ppm", "w")
+    stego = get_ppm_as_list(stego_name)
+    key = get_ppm_as_list(key_name)
 
-    stego_img = embed_imgs(carrier,legit_msg)
+    reveal_img = extract(key, stego)
 
-    for i in range(0,len(stego_img)): 
-            f.write(stego_img[i] + " ")
+    f = open("Steganography/UPRB/images/reveal.ppm", "w")
+
+    for i in range(4):
+        f.write(key[i]+" ")
+    
+    for i in range(len(reveal_img)):
+        f.write(str(reveal_img[i])+" ")
+    
+
+def hide_image(): 
+    carrier_name = input("Enter Carrier name (with extension): ")
+    legit_name = input("Enter the name of image you want to hide (with extension): ")
+
+    carrier_size = os.path.getsize(sys.path[0] + '/UPRB/images/' + carrier_name)
+    legit_msg_size = os.path.getsize(sys.path[0] + '/UPRB/images/'+ legit_name)
+
+    if(carrier_size > legit_msg_size):
+        legit_msg = get_ppm_as_byte_list(legit_name)
+        carrier = get_ppm_as_list(carrier_name)
+
+        stego_img = embed(carrier,legit_msg)
+
+        #write stegoimage file
+        f = open("Steganography/UPRB/images/stegoimage.ppm", "w")
+        for i in range(0,len(stego_img)): 
+                f.write(stego_img[i] + " ")
+                if i%12 == 0:
+                    f.write("\n")
+        print("Image successfully hidden!")
+    else:
+        print("ERROR: Carrier image must be at least 3 times larger than Legit image.")
+
+def show_menu():
+    ans = ""
+    while ans != 0:
+        print("\nWhat do you want to do?")
+        print("1. Hide an Image\n2. Extract an Image\n0. EXIT")
+        ans = int(input("Enter option number: "))
+        if ans == 1:
+            hide_image()
+        elif ans == 2:
+            reveal_image()
+        elif ans != 0:
+            print("\n\033[93m"+ str(ans), "is not a valid input\033[0m")
+            print("\nWhat do you want to do?")
+            print("1. Hide an Image\n2. Extract an Image\n0. EXIT")
+            ans = int(input("Enter option number: "))
+
+def main():
+    show_menu()
 
 if __name__ == "__main__":
     main()
